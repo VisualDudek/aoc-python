218e3b11bb8abae2a511772e6c1ed70e70bc0675
MarcinDudek <dudek.v@gmail.com>
2025-12-02 13:34:05 +0100

    feat: add timing decorator to measure function execution time

--- Y2025/utils.py
+++ Y2025/utils.py
@@ -1,5 +1,18 @@
 from typing import Protocol, List, Tuple
 import numpy as np
+from functools import wraps
+from time import perf_counter
+
+
+def timed(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = perf_counter()
+        result = func(*args, **kwargs)
+        end = perf_counter()
+        print(f"{func.__name__} took: {end - start:.4f} seconds")
+        return result
+    return wrapper
 
 type Data = Tuple[List[int], List[int]]
 
