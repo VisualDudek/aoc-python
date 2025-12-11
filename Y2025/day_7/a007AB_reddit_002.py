eb4d0908b3c2f4d8a1eba49805c8ac006c1cd413
MarcinDudek <dudek.v@gmail.com>
2025-12-11 10:47:32 +0100

    add: implement Beam class and load_grid function for grid processing

---
+++ Y2025/day_7/a007AB_reddit_002.py
@@ -0,0 +1,105 @@
+"""
+- it is correct but do not like code flow
+"""
+from collections import deque, Counter
+from typing import Dict, Tuple
+
+
+type Point = Tuple[int, int]
+
+class Beam:
+    
+    def __init__(self, position: Point) -> None:
+        self.position = position
+
+    # GOTCHA: instead of keeping position as tuple, you could have
+    # Position class with x and y attributes and next_point property/method
+    def drop_down(self) -> None:
+        '''
+        This method does not takes into account splitting
+        '''
+        x, y = self.position
+        self.position = (x, y + 1)
+
+    # GOTCHA: interesting approach is provide method next_beams with point of grid
+    # that will determine if beam splits or not
+    def next_beams(self, char: str) -> list['Beam']:
+        x, y = self.position
+        if char == '^':
+            return [Beam((x - 1, y + 1)), Beam((x + 1, y + 1))]
+        elif char == 'X':
+            return []  # beam is lost
+        else:
+            return [Beam((x, y + 1))]
+        
+
+def load_grid(file_path: str) -> Dict[Point, str]:
+    _grid = {}
+
+    with open(file_path) as f:
+        for y, line in enumerate(f):
+            for x, char in enumerate(line.strip()):
+                # GOTCHA: decision to skip '.' here was stupid BC
+                # later you need to check if point is in grid
+                # if char != '.':
+                _grid[(x, y)] = char
+
+    return _grid
+
+
+# PART 2: getting bottom points
+def get_bottom_points(grid: Dict[Point, str]) -> list[Point]:
+    max_y = max(y for _, y in grid.keys())
+    bottom_points = [point for point in grid.keys() if point[1] == max_y]
+    return bottom_points
+
+
+def main():
+    # file_path = "../puzzle_input/007_test.txt"
+    file_path = "../puzzle_input/007.txt"
+    grid_dict = load_grid(file_path)
+    # GOTCHA: could make Grid a class with methods to get neighbors, etc.
+    grid = grid_dict
+
+    # GOTCHA: this on-liner is crazy for starting point
+    start_point = next(point for point, char in grid.items() if char == 'S')
+
+    # beam_list = [Beam(start_point)]
+
+    # --- trying to recover from previous fail ---
+    beams  = deque([start_point])
+    seen: set[Point] = set()  # GOTCHA: this syntax is interesting
+
+    total_splitters = 0
+
+    # --- Part 2 ---
+    # Timelines
+    timelines = Counter([start_point])
+    bottom_points: list[Point] = get_bottom_points(grid)
+
+    # GOTCHA: I can while on deque directly
+    while beams:
+        c_beam = beams.popleft()  # cSpell:words popleft
+
+        next_beams = Beam(c_beam).next_beams(grid.get(c_beam, 'X'))
+        for nb in next_beams:
+            # This is crucial to avoid multiple counting of same beam position
+            if nb.position not in seen:
+                seen.add(nb.position)
+                beams.append(nb.position)
+
+            # PART 2: Track timelines
+            timelines[nb.position] += timelines[c_beam]
+
+        if len(next_beams) > 1:
+            total_splitters += 1
+
+    num_bottom_timelines = sum(timelines[point] for point in bottom_points)
+
+    print(f"Total splitters encountered: {total_splitters}")
+    print(f"Total timelines reaching bottom points: {num_bottom_timelines}")
+
+
+if __name__ == '__main__':
+    main()
+
