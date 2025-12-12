71013de489f417c553098287e1d790088de3b35e
MarcinDudek <dudek.v@gmail.com>
2025-12-12 14:21:16 +0100

    pending

---
+++ Y2025/day_8/a008B.py
@@ -0,0 +1,97 @@
+"""
+- solution based on NOTES.md file, Approach #2,
+- second approach after getting stuck with cognitive load of managing circuits,
+- using DSU (Disjoint Set Union) data structure to manage circuits
+
+- Solving Part 2
+"""
+from dataclasses import dataclass
+from typing import Dict, Generator, Protocol, List, Tuple, Union
+from itertools import combinations
+from math import dist, prod
+
+type Point = Tuple[int, int, int]
+
+def load(file_path: str) -> List[Point]:
+    _boxes = []
+
+    with open(file_path) as f:
+        for row in f:
+            x, y, z = map(int, row.strip().split(','))
+            _boxes.append((x, y, z))
+
+    return _boxes
+
+
+class DSU:
+    def __init__(self, elements=None):
+        """
+        elements: optional iterable of initial elements
+        """
+        self.parent = {}
+        self.rank = {}
+        # PUZZLE SPECIFIC:
+        self.size = {item: 1 for item in elements} if elements is not None else {}
+
+        # Initially each element is its own parent (self root)
+        if elements is not None:
+            for x in elements:
+                self.parent[x] = x
+                self.rank[x] = 0
+
+    def find(self, x):
+        # lazy initialization for unseen elements
+        # due to puzzle specific usage od self.size below not work properly
+
+        # if x not in self.parent:
+        #     self.parent[x] = x
+        #     self.rank[x] = 0
+        #     return x
+
+        # GOTCHA: path compression -> flattening the tree
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])  # path compression
+        return self.parent[x]
+
+    def union(self, x, y):
+        rx = self.find(x)
+        ry = self.find(y)
+
+        if rx == ry:
+            return False
+
+        # Why do I need union by rank?
+        # union by rank
+        if self.rank[rx] < self.rank[ry]:
+            self.parent[rx] = ry
+            # PUZZLE SPECIFIC:
+            self.size[ry] += self.size[rx]
+            del self.size[rx]
+        elif self.rank[rx] > self.rank[ry]:
+            self.parent[ry] = rx
+            # PUZZLE SPECIFIC:
+            self.size[rx] += self.size[ry]
+            del self.size[ry]
+        else:
+            self.parent[ry] = rx
+            self.rank[rx] += 1
+            self.size[rx] += self.size[ry]
+            del self.size[ry]
+
+        return True
+
+
+def main():
+    # file_path = "../puzzle_input/008_test.txt"
+    file_path = "../puzzle_input/008.txt"
+    boxes: List[Point] = load(file_path)
+
+    data = sorted(combinations(boxes, 2), key=lambda pair: dist(*pair))
+
+    x, y = data[-1]
+    res = x[0] * y[0]
+    print(f"Result: {res}")
+
+
+if __name__ == '__main__':
+    main()
